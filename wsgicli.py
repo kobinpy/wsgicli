83ed6e366680b3c1de49f80637d410179ea7fff0
c-bata <contact@c-bata.link>
2016-11-29 23:19:29 +0900

    Add click.argument

--- wsgicli.py
+++ wsgicli.py
@@ -1,21 +1,22 @@
 import click
 from wsgi_static_middleware import StaticMiddleware
 from wsgiref.simple_server import make_server
-import importlib
+from importlib.machinery import SourceFileLoader
 
 
-@click.command('run', short_help='Runs a development server.')
-@click.argument('wsgi')
+@click.command()
+@click.argument('file_path', type=click.File(), nargs=1)
+@click.argument('wsgi_app', nargs=1)
 @click.option('--host', '-h', default='127.0.0.1', help='The interface to bind to.')
 @click.option('--port', '-p', default=8000, help='The port to bind to.')
 @click.option('--enable-static/--disable-static', default=None, help='Static file serving')
 @click.option('--static-root', default='static', help='Static root')
 @click.option('--static-dir', default='./static/', multiple=True,
               help='Directories for static files')
-def cmd(wsgi, host, port, enable_static, static_root, static_dir, verbose):
+def cmd(file_path, wsgi_app, host, port, enable_static, static_root, static_dir):
     """Runs a development server for WSGI Application"""
-    file, obj = wsgi.split(':')
-    app = getattr(importlib.import_module(file), obj)
+    module = SourceFileLoader('app', filepath).load_module()
+    app = getattr(module, wsgiapp)
     if enable_static:
         app = StaticMiddleware(app, static_root=static_root, static_dirs=static_dir)
     httpd = make_server(host, port, app)
