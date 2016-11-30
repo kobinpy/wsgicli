e078aae6c480562b92848148ed0716835214f8dc
c-bata <contact@c-bata.link>
2016-12-01 02:10:40 +0900

    Release v0.1.0 :tada:

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ from setuptools import setup
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(BASE_PATH, 'README.rst')).read()
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __author__ = 'Masashi Shibata <contact@c-bata.link>'
 __author_email__ = 'contact@c-bata.link'
 __license__ = 'MIT License'
 __classifiers__ = (
-    'Development Status :: 1 - Planning',
+    'Development Status :: 2 - Pre-Alpha',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Topic :: Internet :: WWW/HTTP :: WSGI',
     'Environment :: Web Environment',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
@@ -29,7 +30,7 @@ setup(
     author=__author__,
     author_email=__author_email__,
     url='https://github.com/c-bata/wsgicli',
-    description='',
+    description='Command Line Interface for developing WSGI application.',
     long_description=README,
     classifiers=__classifiers__,
     py_modules=['wsgicli'],
